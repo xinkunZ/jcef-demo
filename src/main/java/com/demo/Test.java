28cfd95e25f4ae18fe8f6bed6ebf4603550534d4
zhangxinkun <zhangxinkun@flashhold.com>
2022-06-16 19:11:31 +0800

    update

---
+++ src/main/java/com/demo/Test.java
@@ -0,0 +1,31 @@
+package com.demo;
+
+
+/**
+ * @author zhangxinkun
+ */
+public class Test {
+
+    public static void main(String[] args)  {
+        //-Djava.library.path="C:\Users\zhang\AppData\Local\Temp\journey-78"
+//        JourneyBrowserView browser = new JourneyBrowserView("https://google.com");
+//        JFrame frame = new JFrame();
+//        frame.setLayout(new BorderLayout());
+//        frame.setContentPane(browser);
+//
+//        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+//        frame.addWindowListener(new WindowAdapter() {
+//            @Override
+//            public void windowClosing(WindowEvent e) {
+//                browser.getCefApp().dispose();
+//                frame.dispose();
+//            }
+//        });
+//
+//        frame.setTitle("Journey");
+//        frame.setSize(1000, 600);
+//
+//        frame.setVisible(true);
+    }
+
+}
