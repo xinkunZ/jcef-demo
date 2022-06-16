8b8ad5b598fd14eb7e8621d09c1eb42f2f3e9474
zhangxinkun <zhangxinkun@flashhold.com>
2022-06-16 19:22:34 +0800

    use jbr

--- src/main/java/com/demo/T.java
+++ src/main/java/com/demo/T.java
@@ -1,11 +1,5 @@
 package com.demo;
 
-import org.cef.browser.CefBrowser;
-import org.panda_lang.pandomium.Pandomium;
-import org.panda_lang.pandomium.wrapper.PandomiumClient;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * @author zhangxinkun
@@ -15,12 +9,12 @@ public final class T {
     public static void main(String[] args) {
         // setting this option is paramount otherwise no cache is kept, and sesions are lost
 
-        Pandomium pandomium = Pandomium.buildDefault();
-        final CefBrowser cefBrowser = pandomium.createClient().loadURL("http://172.31.239.153");
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(cefBrowser.getUIComponent(), BorderLayout.CENTER);
-        frame.setTitle("Whatsapp");
-        frame.setSize(1720, 840);
-        frame.setVisible(true);
+//        Pandomium pandomium = Pandomium.buildDefault();
+//        final CefBrowser cefBrowser = pandomium.createClient().loadURL("http://172.31.239.153");
+//        JFrame frame = new JFrame();
+//        frame.getContentPane().add(cefBrowser.getUIComponent(), BorderLayout.CENTER);
+//        frame.setTitle("Whatsapp");
+//        frame.setSize(1720, 840);
+//        frame.setVisible(true);
     }
 }
--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -1,31 +1,45 @@
 package com.demo;
 
 
+import com.jetbrains.cef.JCefAppConfig;
+import org.cef.CefApp;
+import org.cef.browser.CefBrowser;
+import org.cef.browser.CefRendering;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * @author zhangxinkun
  */
 public class Test {
 
     public static void main(String[] args)  {
-        //-Djava.library.path="C:\Users\zhang\AppData\Local\Temp\journey-78"
+//        -Djava.library.path="C:\Users\zhang\AppData\Local\Temp\journey-78"
 //        JourneyBrowserView browser = new JourneyBrowserView("https://google.com");
-//        JFrame frame = new JFrame();
-//        frame.setLayout(new BorderLayout());
-//        frame.setContentPane(browser);
-//
-//        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-//        frame.addWindowListener(new WindowAdapter() {
-//            @Override
-//            public void windowClosing(WindowEvent e) {
-//                browser.getCefApp().dispose();
-//                frame.dispose();
-//            }
-//        });
-//
-//        frame.setTitle("Journey");
-//        frame.setSize(1000, 600);
-//
-//        frame.setVisible(true);
+        final JCefAppConfig instance = JCefAppConfig.getInstance();
+        final CefApp app = CefApp.getInstance();
+        final CefBrowser browser = app.createClient()
+                .createBrowser("https://google.com", CefRendering.DEFAULT, false);
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(browser.getUIComponent(),BorderLayout.CENTER);
+
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                app.dispose();
+                frame.dispose();
+            }
+        });
+
+        frame.setTitle("Journey");
+        frame.setSize(1000, 600);
+
+        frame.setVisible(true);
     }
 
 }
