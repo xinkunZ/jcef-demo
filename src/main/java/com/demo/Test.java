a1224600c7e5ab9d5b763b78915b1587775366a9
zhangxinkun <zhangxinkun@flashhold.com>
2022-06-29 10:45:40 +0800

    套壳成功

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -1,6 +1,7 @@
 package com.demo;
 
 
+import com.google.common.collect.Lists;
 import com.jetbrains.cef.JCefAppConfig;
 import org.cef.CefApp;
 import org.cef.browser.CefBrowser;
@@ -10,22 +11,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author zhangxinkun
  */
 public class Test {
 
-    public static void main(String[] args)  {
-//        -Djava.library.path="C:\Users\zhang\AppData\Local\Temp\journey-78"
-//        JourneyBrowserView browser = new JourneyBrowserView("https://google.com");
+    public static void main(String[] args) {
         final JCefAppConfig instance = JCefAppConfig.getInstance();
-        final CefApp app = CefApp.getInstance(instance.getAppArgs(),instance.getCefSettings());
-//        "http://172.31.239.153"
-        String url="https://www.apple.com";
+        final String[] appArgs = instance.getAppArgs();
+
+        final List<String> allArgs = Lists.newArrayList();
+        allArgs.addAll(Arrays.asList(appArgs));
+        allArgs.addAll(Arrays.asList(args));
+
+        final String[] array = allArgs.toArray(String[]::new);
+        CefApp.startup(array);
+        final CefApp app = CefApp.getInstance(array, instance.getCefSettings());
+
+        String url = "http://172.31.238.181";
         final CefBrowser browser = app.createClient().createBrowser(url, CefRendering.DEFAULT, false);
         JFrame frame = new JFrame();
-        frame.getContentPane().add(browser.getUIComponent(),BorderLayout.CENTER);
+        frame.getContentPane().add(browser.getUIComponent(), BorderLayout.CENTER);
         frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -37,7 +46,6 @@ public class Test {
 
         frame.setTitle("demo");
         frame.setSize(1000, 600);
-
         frame.setVisible(true);
     }
 
